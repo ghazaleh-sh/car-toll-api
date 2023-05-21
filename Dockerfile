7e21bc55243d30076c2b96a717479bc341f99e95
g.shahrokhabadi <G.ShahrokhAbadi@sadad.co.ir>
2023-05-21 15:59:57 +0330

    docker modified

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
+FROM eclipse-temurin:8u362-b09-jdk-alpine
 
 VOLUME /tmp
 
@@ -14,4 +14,4 @@ COPY src/main/resources/public.pem /opt/security/car-toll-api/
 
 
 COPY target/*.jar car-toll-api-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-Xdebug","-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1514","-jar","/car-toll-api-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java","-Xdebug","-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1514","-Dcom.ibm.icu.util.TimeZone.DefaultTimeZoneType=JDK","-jar","/car-toll-api-0.0.1-SNAPSHOT.jar"]
--- src/main/java/ir/co/sadad/cartollapi/CarTollApiApplication.java
+++ src/main/java/ir/co/sadad/cartollapi/CarTollApiApplication.java
@@ -1,14 +1,21 @@
 package ir.co.sadad.cartollapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
+@Slf4j
 @EnableJpaAuditing
 @SpringBootApplication
 public class CarTollApiApplication {
 
 	public static void main(String[] args) {
+		log.warn("Jdk DST offset is: {}", Calendar.getInstance(TimeZone.getDefault()).get(Calendar.DST_OFFSET));
+		log.warn("ICU4J DST offset is: {}", com.ibm.icu.util.Calendar.getInstance(com.ibm.icu.util.TimeZone.getDefault()).get(com.ibm.icu.util.Calendar.DST_OFFSET));
 		SpringApplication.run(CarTollApiApplication.class, args);
 	}
 
